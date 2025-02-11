d7d1930d298b6afb77ebf0b9791bd9178eb1471f
psh <todo220@naver.com>
2025-02-11 16:58:00 +0900

    hotfix branches 내용 추가

--- java_basic/src/pack/HelloTest.java
+++ java_basic/src/pack/HelloTest.java
@@ -4,7 +4,7 @@ public class HelloTest {
 	public static void main(String[] args) {
 		System.out.println("프로그램 작성");
 		System.out.println("hotfix branches 내용 변경");
-		
+		System.out.println("hotfix branches 내용 추가");
 		
 		
 		
